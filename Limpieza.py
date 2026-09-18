370f8db8d1a4876917ddfac3a6b7030fdd5fc160
JosueSV <josue.sifuentesvaldez@gmail.com>
2026-09-18 08:54:16 -0600

    Update Limpieza.py
    Se completo la limpieza y se empezo a clasificar los datos

--- Limpieza.py
+++ Limpieza.py
@@ -22,3 +22,17 @@ print("Duplicados:", df.duplicated().sum()) #Esto nos sirve para saber si hay re
 
 print(df.describe()) #Muestra estadísticas descriptivas del df
 
+
+# LIMPIEZA DE DATOS
+df["average_score"] = df[["math score", "reading score", "writing score"]].mean(axis=1) #Se crea la columna promedio que es el promedio de las 3 columnas de calificaciones
+df["average_score"]= df["average_score"].round(2) #Se redondea a 2 decimales la columna promedio
+print(df.head())
+
+
+# CLASIFICACION
+df["rendimiento"] = pd.cut(
+    df["average_score"], 
+    bins=[0, 60, 80, 100], #Se clasifican los rangos
+    labels=["Bajo", "Medio", "Alto"]) #Aqui cree la columna rendimiento que clasifica el promedio en Bajo, Medio y Alto
+
+print(df.head())
